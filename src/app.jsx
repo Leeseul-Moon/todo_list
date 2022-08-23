cf1df09ca324bb4fa1739b54a371c42028468783
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-08-24 00:48:47 +0900

    test deploy

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
 import styles from "./app.module.css";
 import Detail from "./components/detail/detail";
 import Layout from "./components/layout/layout";
@@ -6,12 +6,12 @@ import Layout from "./components/layout/layout";
 function App() {
   return (
     <div className={styles.app}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes basename={process.env.PUBLIC_URL}>
           <Route path="/" exact element={<Layout />} />
           <Route path="/detail/:id" element={<Detail />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
